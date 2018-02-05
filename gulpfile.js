b7a8eb2fb5408cdaad969ea1b103a19748f59c99
Irina <irina-aliyeva@yandex.ru>
2018-02-05 03:03:41 +0300

    added styles gulp task-dz-05-03

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,20 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
 
-gulp.task('styles', ['less'], function() {
+gulp.task('server', ['styles'], function() {
     browserSync.init({
     	server: { baseDir: './app/'}
     });
     gulp.watch('./app/**/*.html').on('change', browserSync.reload);
     // gulp.watch('./app/less/**/*.less', ['less']);
-    gulp.watch('./app/less/**/*.less', ['less']);
+    gulp.watch('./app/less/**/*.less', ['styles']);
 });
 
-gulp.task('less', function() {
+gulp.task('styles', function() {
     return gulp.src('./app/less/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('./app/css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['server']);
